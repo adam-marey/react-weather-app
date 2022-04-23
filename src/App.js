62ad8757894ceb995977ea90985798aab870c09d
Sulaiman Marey <adammarey51@gmail.com>
2022-04-23 14:13:35 -0700

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
 function App() {
   // const url = `https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=5ca424da57a921f39815ec62e1ffd863`
