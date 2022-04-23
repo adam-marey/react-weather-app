3d0e223e5417e146d641464ff787b93872923f5b
Sulaiman Marey <adammarey51@gmail.com>
2022-04-23 14:10:59 -0700

    added weather api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import axios from 'axios'
 function App() {
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=5ca424da57a921f39815ec62e1ffd863`
+
   return (
-    <div className="App"> hello world
+    <div className="app"> hello world
     </div>
   );
 }
