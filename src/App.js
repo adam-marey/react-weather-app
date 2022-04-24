b1233268ec2309477651d491ea2843eee70ae36e
Sulaiman Marey <adammarey51@gmail.com>
2022-04-23 18:11:40 -0700

    added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, {useState} from 'react'
-import axios from 'axios'
+import axios from 'axios';
+
 function App() {
+  const [data, setData] = useState({})
+  const [location, useLocation]
   // const url = `https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=5ca424da57a921f39815ec62e1ffd863`
 
   return (
-    <div className="app"> hello world
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Phoenix</p>
+          </div>
+          <div className="temp">
+            <h1>60 ºƒ</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65 ºƒ</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12 MPH
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
